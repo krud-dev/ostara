60d50b44fbbbecc01851999d935f63ac143d8c49
avivm <mo.aviv@gmail.com>
2023-01-02 17:52:44 +0200

    feat: update instance uptime manually every second

--- src/renderer/components/widget/metric/CountdownValue.tsx
+++ src/renderer/components/widget/metric/CountdownValue.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useHarmonicIntervalFn } from 'react-use';
 
 export type CountdownValueProps = {
   seconds: number;
 };
 
 const CountdownValue = ({ seconds }: CountdownValueProps) => {
-  const d = useMemo<number>(() => Math.floor(seconds / 86400), [seconds]);
-  const h = useMemo<number>(() => Math.floor((seconds % 86400) / 3600), [seconds]);
-  const m = useMemo<number>(() => Math.floor((seconds % 3600) / 60), [seconds]);
-  const s = useMemo<number>(() => Math.floor(seconds % 60), [seconds]);
+  const [tickFlag, setTickFlag] = useState<boolean>(false);
+
+  const inputTime = useMemo<number>(() => Date.now() - seconds * 1000, [seconds]);
+  const countdownSeconds = useMemo<number>(() => (Date.now() - inputTime) / 1000, [inputTime, tickFlag]);
+
+  const padValue = useCallback((value: number): string => (value < 10 ? `0${value}` : `${value}`), []);
+
+  const d = useMemo<number>(() => Math.floor(countdownSeconds / 86400), [countdownSeconds]);
+  const h = useMemo<string>(() => padValue(Math.floor((countdownSeconds % 86400) / 3600)), [countdownSeconds]);
+  const m = useMemo<string>(() => padValue(Math.floor((countdownSeconds % 3600) / 60)), [countdownSeconds]);
+  const s = useMemo<string>(() => padValue(Math.floor(countdownSeconds % 60)), [countdownSeconds]);
+
+  useHarmonicIntervalFn(() => {
+    setTickFlag((flag) => !flag);
+  }, 1000);
 
   return (
     <>
