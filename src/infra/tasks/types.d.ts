654d1feae1a01582123de42e6f2fc0c23b394b69
Idan Elhalwani <idan@elhalwani.com>
2023-01-03 08:21:00 +0200

    fix: add task bridge to window

--- src/infra/tasks/types.d.ts
+++ src/infra/tasks/types.d.ts
@@ -1,3 +1,14 @@
+import {
+  Application,
+  Configuration,
+  EnrichedApplication,
+  EnrichedFolder,
+  EnrichedInstance,
+  EnrichedItem,
+  Folder,
+  Instance,
+} from '../configuration/model/configuration';
+
 export type TaskName = 'queryInstanceMetrics' | 'queryInstanceHealth';
 
 export type TaskDefinition = {
@@ -17,7 +28,15 @@ export type TaskDefinitionDisplay = Omit<EffectiveTaskDefinition, 'function'> & 
   nextRun: number;
 };
 
-export type TaskServiceBridge = {
-  getTasksForDisplay: () => Promise<TaskDefinitionDisplay[]>;
-  getTaskForDisplay: (name: string) => Promise<TaskDefinitionDisplay | undefined>;
-};
+declare global {
+  export type TaskServiceBridge = {
+    getTasksForDisplay: () => Promise<TaskDefinitionDisplay[]>;
+    getTaskForDisplay: (name: string) => Promise<TaskDefinitionDisplay | undefined>;
+  };
+
+  interface Window {
+    task: TaskServiceBridge;
+  }
+}
+
+export {};
