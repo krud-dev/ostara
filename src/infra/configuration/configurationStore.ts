e5c2b1148b51519d8d4647c47c8fcbb870b351c8
Idan Elhalwani <idan@elhalwani.com>
2023-01-09 15:50:02 +0200

    fix: add default value to configuration

--- src/infra/configuration/configurationStore.ts
+++ src/infra/configuration/configurationStore.ts
@@ -1,6 +1,10 @@
 import ElectronStore from 'electron-store';
 import { Configuration } from './model/configuration';
 
+const defaults: Configuration = {
+  items: {},
+};
 export const configurationStore = new ElectronStore<Configuration>({
   name: 'boost_configuration',
+  defaults,
 });
