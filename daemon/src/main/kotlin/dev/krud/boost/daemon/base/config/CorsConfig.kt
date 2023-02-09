96115f67b940f3d56a529c4aee6ab5fef56f9b94
Idan Elhalwani <idan@elhalwani.com>
2023-02-09 18:07:41 +0200

    feat: add temp cors config

---
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package dev.krud.boost.daemon.base.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
