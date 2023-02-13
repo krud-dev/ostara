6717960707c2381c16de9547565fba4b7de715ee
Idan Elhalwani <idan@elhalwani.com>
2023-02-13 18:28:20 +0200

    fix: change error page to include message

--- daemon/src/main/kotlin/dev/krud/boost/daemon/exception/_util.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/exception/_util.kt
@@ -1,10 +1,10 @@
 package dev.krud.boost.daemon.exception
 
 import org.springframework.http.HttpStatus
-import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.server.ResponseStatusException
 
-fun throwBadRequest(message: String): Nothing = throw HttpClientErrorException(HttpStatus.BAD_REQUEST, message)
+fun throwBadRequest(message: String): Nothing = throw ResponseStatusException(HttpStatus.BAD_REQUEST, message)
 
-fun throwNotFound(message: String): Nothing = throw HttpClientErrorException(HttpStatus.NOT_FOUND, message)
+fun throwNotFound(message: String): Nothing = throw ResponseStatusException(HttpStatus.NOT_FOUND, message)
 
-fun throwInternalServerError(message: String): Nothing = throw HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, message)
+fun throwInternalServerError(message: String): Nothing = throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, message)
