3b4cdef1272439aa764029093b6b9d5437862e69
Idan Elhalwani <idan@elhalwani.com>
2023-02-14 15:50:19 +0200

    feat: remove item specific events

--- src/infra/events/events.ts
+++ src/infra/events/events.ts
@@ -1,5 +1,3 @@
-import { Application, Instance } from '../configuration/model/configuration';
-
 export type Events = {
   /**
    * System
@@ -7,34 +5,4 @@ export type Events = {
   'daemon-ready': () => void;
   'daemon-healthy': () => void;
   'daemon-unhealthy': () => void;
-  /**
-   * instance events
-   * @param
-   */
-  'instance-created': (instance: Instance) => void;
-  'instance-updated': (instance: Instance) => void;
-  'instance-deleted': (instance: Instance) => void;
-  'instance-moved': (instance: Instance, oldParentApplicationId: string, newParentApplicationId: string) => void;
-  /**
-   * Application events
-   * @param application
-   */
-  'application-created': (application: Application) => void;
-  'application-updated': (application: Application) => void;
-  'application-deleted': (application: Application) => void;
-  'application-moved': (
-    application: Application,
-    oldParentFolderId: string | undefined,
-    newParentFolderId: string | undefined
-  ) => void;
-
-  /**
-   * Heapdump Reference events
-   */
-
-  'heapdump-reference-downloading': (referenceId: string) => void;
-
-  'heapdump-reference-download-complete': (referenceId: string) => void;
-
-  'heapdump-reference-download-failed': (referenceId: string, message: string) => void;
 };
