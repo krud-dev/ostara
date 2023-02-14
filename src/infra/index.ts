73a545e235bf922390b797262f3a97fb7192ce4f
Idan Elhalwani <idan@elhalwani.com>
2023-02-14 15:43:10 +0200

    feat: remove metrics

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -2,6 +2,5 @@ import './daemon';
 import './rendererUtils';
 import './configuration';
 import './actuator';
-import './metrics';
 import './instance';
 import './ui';
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,7 +1,6 @@
 import { contextBridge } from 'electron';
 import { actuatorBridge } from '../infra/actuator/renderer';
 import { configurationServiceBridge, configurationStoreBridge } from '../infra/configuration/renderer';
-import { metricsServiceBridge } from '../infra/metrics/renderer';
 import { utilsBridge } from '../infra/rendererUtils/renderer';
 import { subscriptionsBridge } from '../infra/subscriptions/renderer';
 import { instanceServiceBridge } from '../infra/instance/renderer';
@@ -13,7 +12,6 @@ contextBridge.exposeInMainWorld('electron', {
 });
 contextBridge.exposeInMainWorld('actuator', actuatorBridge);
 contextBridge.exposeInMainWorld('configuration', configurationServiceBridge);
-contextBridge.exposeInMainWorld('metrics', metricsServiceBridge);
 contextBridge.exposeInMainWorld('utils', utilsBridge);
 contextBridge.exposeInMainWorld('subscriptions', subscriptionsBridge);
 contextBridge.exposeInMainWorld('instance', instanceServiceBridge);
