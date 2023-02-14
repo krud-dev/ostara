c10ebf0ff44a3f8a59759b66945450a55499e65c
Idan Elhalwani <idan@elhalwani.com>
2023-02-14 15:49:37 +0200

    feat: remove actuator passthrough

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,6 +1,5 @@
 import './daemon';
 import './rendererUtils';
 import './configuration';
-import './actuator';
 import './instance';
 import './ui';
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,5 +1,4 @@
 import { contextBridge } from 'electron';
-import { actuatorBridge } from '../infra/actuator/renderer';
 import { configurationServiceBridge, configurationStoreBridge } from '../infra/configuration/renderer';
 import { utilsBridge } from '../infra/rendererUtils/renderer';
 import { subscriptionsBridge } from '../infra/subscriptions/renderer';
@@ -10,7 +9,6 @@ import { daemonAddressSupplier } from '../infra/daemon/renderer';
 contextBridge.exposeInMainWorld('electron', {
   configurationStore: configurationStoreBridge,
 });
-contextBridge.exposeInMainWorld('actuator', actuatorBridge);
 contextBridge.exposeInMainWorld('configuration', configurationServiceBridge);
 contextBridge.exposeInMainWorld('utils', utilsBridge);
 contextBridge.exposeInMainWorld('subscriptions', subscriptionsBridge);
