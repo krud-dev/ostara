8b7eeba36b32cdc822513bcb8e38e36e54bd9869
Idan Elhalwani <idan@elhalwani.com>
2023-02-14 15:54:48 +0200

    feat: remove irrelevant subscriptions

--- src/infra/subscriptions/subscriptions.ts
+++ src/infra/subscriptions/subscriptions.ts
@@ -1,12 +1,6 @@
-import { InstanceHealth } from '../configuration/model/configuration';
 import { IpcRendererEvent } from 'electron';
-import { InstanceAbility } from '../instance/models/ability';
 import { ElectronTheme } from '../ui/models/electronTheme';
 
 export type Subscriptions = {
-  'app:instanceHealthUpdated': (event: IpcRendererEvent, instanceId: string, health: InstanceHealth) => void;
-  'app:applicationHealthUpdated': (event: IpcRendererEvent, applicationId: string, health: InstanceHealth) => void;
-  'app:instanceEndpointsUpdated': (event: IpcRendererEvent, instanceId: string, endpoints: string[]) => void;
-  'app:instanceAbilitiesUpdated': (event: IpcRendererEvent, instanceId: string, abilities: InstanceAbility[]) => void;
   'app:themeUpdated': (event: IpcRendererEvent, data: ElectronTheme) => void;
 };
