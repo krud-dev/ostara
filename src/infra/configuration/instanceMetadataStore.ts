51ada67c10d5f971ec59619078715cb2f1d2713e
Idan Elhalwani <idan@elhalwani.com>
2023-02-14 15:54:38 +0200

    feat: remove configuration

--- src/infra/configuration/instanceMetadataStore.ts
+++
@@ -1,6 +0,0 @@
-import ElectronStore from 'electron-store';
-import { InstanceMetadata } from './model/configuration';
-
-export const instanceMetadataStore = new ElectronStore<{ [key: string]: InstanceMetadata }>({
-  name: 'boost_instance_data',
-});
