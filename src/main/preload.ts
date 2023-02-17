6e0980ef81e96fc9f5cad2fc8324be070a0cd4c5
Idan Elhalwani <idan@elhalwani.com>
2023-02-17 09:42:09 +0200

    feat: pass NODE_ENV to renderer
    closes #165

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -9,3 +9,4 @@ contextBridge.exposeInMainWorld('subscriptions', subscriptionsBridge);
 contextBridge.exposeInMainWorld('ui', uiServiceBridge);
 contextBridge.exposeInMainWorld('isElectron', true);
 contextBridge.exposeInMainWorld('daemonAddress', daemonAddressSupplier());
+contextBridge.exposeInMainWorld('NODE_ENV', process.env.NODE_ENV);
--- src/main/types.d.ts
+++ src/main/types.d.ts
@@ -1,6 +1,7 @@
 declare global {
   interface Window {
     isElectron: boolean;
+    NODE_ENV: string;
   }
 }
 
