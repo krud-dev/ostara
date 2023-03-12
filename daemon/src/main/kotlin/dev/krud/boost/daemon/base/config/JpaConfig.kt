ee944894211f844ffd8b3e5afe478d16a5dd6baa
Idan Elhalwani <idan@elhalwani.com>
2023-03-12 08:59:15 +0200

    feat: add experimental krud

--- daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/JpaConfig.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/JpaConfig.kt
@@ -1,8 +1,10 @@
 package dev.krud.boost.daemon.base.config
 
+import dev.krud.crudframework.crud.handler.krud.EnableKrud
 import dev.krud.crudframework.jpa.annotation.EnableJpaCrud
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableJpaCrud
+@EnableKrud
 class JpaConfig
