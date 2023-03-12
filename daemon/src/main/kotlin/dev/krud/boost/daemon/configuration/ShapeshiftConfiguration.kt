aa3098cb5c868dcf89d8c1ec77e815d3b2cede67
Idan Elhalwani <idan@elhalwani.com>
2023-03-12 08:59:39 +0200

    feat: add type defaults

--- daemon/src/main/kotlin/dev/krud/boost/daemon/configuration/ShapeshiftConfiguration.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/configuration/ShapeshiftConfiguration.kt
@@ -17,28 +17,28 @@ import dev.krud.boost.daemon.threadprofiling.enums.ThreadProfilingStatus
 import dev.krud.boost.daemon.threadprofiling.model.ThreadProfilingRequest
 import dev.krud.boost.daemon.threadprofiling.ro.ThreadProfilingLogRO
 import dev.krud.boost.daemon.threadprofiling.ro.ThreadProfilingRequestRO
+import dev.krud.boost.daemon.utils.TypeDefaults
 import dev.krud.shapeshift.ShapeShiftBuilder
 import dev.krud.shapeshift.spring.ShapeShiftAutoConfiguration
 import dev.krud.shapeshift.spring.ShapeShiftBuilderCustomizer
 import org.springframework.context.annotation.Configuration
-import java.util.*
 
 @Configuration
 class ShapeshiftConfiguration : ShapeShiftBuilderCustomizer, ShapeShiftAutoConfiguration() {
     override fun customize(builder: ShapeShiftBuilder) {
         builder
-            .withObjectSupplier { FolderModifyRequestRO("") }
-            .withObjectSupplier { FolderRO(UUID.randomUUID(), "") }
-            .withObjectSupplier { ApplicationModifyRequestRO("", ApplicationType.SPRING_BOOT) }
-            .withObjectSupplier { ApplicationRO(UUID.randomUUID(), "", ApplicationType.SPRING_BOOT) }
-            .withObjectSupplier { InstanceModifyRequestRO("", "", UUID.randomUUID()) }
-            .withObjectSupplier { InstanceRO(UUID.randomUUID(), "", null, null, "", UUID.randomUUID()) }
-            .withObjectSupplier { EventLogRO(UUID.randomUUID(), Date(), EventLogType.INSTANCE_HEALTH_CHANGED, EventLogSeverity.INFO, UUID.randomUUID()) }
-            .withObjectSupplier { InstanceHealthLogRO(Date(), UUID.randomUUID()) }
-            .withObjectSupplier { InstanceHeapdumpReference(UUID.randomUUID()) }
-            .withObjectSupplier { InstanceHeapdumpReferenceRO(UUID.randomUUID(), UUID.randomUUID(), Date()) }
-            .withObjectSupplier { ThreadProfilingRequest(UUID.randomUUID(), 0, ThreadProfilingStatus.RUNNING) }
-            .withObjectSupplier { ThreadProfilingRequestRO(UUID.randomUUID(), Date(), UUID.randomUUID(), 0, Date(), ThreadProfilingStatus.RUNNING) }
-            .withObjectSupplier { ThreadProfilingLogRO(UUID.randomUUID(), Date(), UUID.randomUUID()) }
+            .withObjectSupplier { FolderModifyRequestRO(TypeDefaults.STRING) }
+            .withObjectSupplier { FolderRO(TypeDefaults.UUID, TypeDefaults.STRING) }
+            .withObjectSupplier { ApplicationModifyRequestRO(TypeDefaults.STRING, ApplicationType.SPRING_BOOT) }
+            .withObjectSupplier { ApplicationRO(TypeDefaults.UUID, TypeDefaults.STRING, ApplicationType.SPRING_BOOT) }
+            .withObjectSupplier { InstanceModifyRequestRO(TypeDefaults.STRING, TypeDefaults.STRING, TypeDefaults.UUID) }
+            .withObjectSupplier { InstanceRO(TypeDefaults.UUID, TypeDefaults.STRING, null, null, TypeDefaults.STRING, TypeDefaults.UUID) }
+            .withObjectSupplier { EventLogRO(TypeDefaults.UUID, TypeDefaults.DATE, EventLogType.INSTANCE_HEALTH_CHANGED, EventLogSeverity.INFO, TypeDefaults.UUID) }
+            .withObjectSupplier { InstanceHealthLogRO(TypeDefaults.DATE, TypeDefaults.UUID) }
+            .withObjectSupplier { InstanceHeapdumpReference(TypeDefaults.UUID) }
+            .withObjectSupplier { InstanceHeapdumpReferenceRO(TypeDefaults.UUID, TypeDefaults.UUID, TypeDefaults.DATE) }
+            .withObjectSupplier { ThreadProfilingRequest(TypeDefaults.UUID, TypeDefaults.INT, ThreadProfilingStatus.RUNNING) }
+            .withObjectSupplier { ThreadProfilingRequestRO(TypeDefaults.UUID, TypeDefaults.DATE, TypeDefaults.UUID, TypeDefaults.INT, TypeDefaults.DATE, ThreadProfilingStatus.RUNNING) }
+            .withObjectSupplier { ThreadProfilingLogRO(TypeDefaults.UUID, TypeDefaults.DATE, TypeDefaults.UUID) }
     }
 }
---
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/utils/TypeDefaults.kt
@@ -0,0 +1,17 @@
+package dev.krud.boost.daemon.utils
+
+import java.util.*
+
+object TypeDefaults {
+    val UUID = UUID(0L, 0L)
+    val DATE = Date(0L)
+    val STRING = ""
+    val INT = 0
+    val LONG = 0L
+    val FLOAT = 0.0f
+    val DOUBLE = 0.0
+    val BOOLEAN = false
+    val CHAR = ' '
+    val BYTE = 0.toByte()
+    val SHORT = 0.toShort()
+}
