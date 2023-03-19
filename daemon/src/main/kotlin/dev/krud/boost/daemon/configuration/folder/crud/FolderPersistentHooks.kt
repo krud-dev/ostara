1d14f693b36fcaa3c14976be90993ae5e3f1361a
Idan Elhalwani <idan@elhalwani.com>
2023-03-19 15:46:46 +0200

    feat: upgrade crud version

--- daemon/src/main/kotlin/dev/krud/boost/daemon/configuration/folder/crud/FolderPersistentHooks.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/configuration/folder/crud/FolderPersistentHooks.kt
@@ -6,24 +6,21 @@ import dev.krud.crudframework.crud.handler.krud.Krud
 import dev.krud.crudframework.crud.hooks.interfaces.DeleteHooks
 import org.springframework.stereotype.Component
 import java.util.*
-import kotlin.reflect.KProperty
-import kotlin.reflect.KProperty1
 
 @Component
 class FolderPersistentHooks(
     private val folderKrud: Krud<Folder, UUID>,
     private val applicationKrud: Krud<Application, UUID>
 ) : DeleteHooks<UUID, Folder> {
-    // Temp fix until crud deployment is fixed
     override fun onDelete(entity: Folder) {
         folderKrud.deleteByFilter {
             where {
-                (Folder::parentFolderId as KProperty1<Folder, UUID>) Equal entity.id
+                Folder::parentFolderId Equal entity.id
             }
         }
         applicationKrud.deleteByFilter {
             where {
-                (Application::parentFolderId as KProperty1<Application, UUID>) Equal entity.id
+                Application::parentFolderId Equal entity.id
             }
         }
     }
