dabca35f4425e93a7629f5c5114eb7b50bea62ac
Idan Elhalwani <idan@elhalwani.com>
2023-04-04 11:03:49 +0300

    Revert "chore: add signed sqlite driver for production builds"
    This reverts commit 184c210d4adc47c394eae0c93c256c653e5a0cff.

--- daemon/build.gradle.kts
+++ daemon/build.gradle.kts
@@ -28,7 +28,6 @@ repositories {
 }
 
 val implementationDev by configurations.creating
-val implementationProd by configurations.creating
 
 if (!project.hasProperty("prod")) {
     configurations {
@@ -63,12 +62,6 @@ if (!project.hasProperty("prod")) {
         nullabilityDefinition = cz.habarta.typescript.generator.NullabilityDefinition.undefinedInlineUnion
         mapDate = cz.habarta.typescript.generator.DateMapping.asNumber
     }
-} else {
-    configurations {
-        implementation {
-            extendsFrom(implementationProd)
-        }
-    }
 }
 
 
@@ -77,9 +70,6 @@ dependencies {
 
     implementationDev("org.springframework.boot:spring-boot-starter-actuator")
     implementationDev("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
-    implementationDev("org.xerial:sqlite-jdbc:3.40.0.0")
-
-    implementationProd(files("lib/sqlite-jdbc-3.40.0.0.jar"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -92,6 +82,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.hibernate.orm:hibernate-community-dialects")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.xerial:sqlite-jdbc:3.40.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("org.flywaydb:flyway-core:9.12.0")
     implementation("dev.krud:crud-framework-core:$crudFrameworkVersion")
