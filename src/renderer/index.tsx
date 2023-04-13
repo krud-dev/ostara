e17c4b392ffb6ef461b2bff9d94f42086fc1d94f
Idan Elhalwani <idan@elhalwani.com>
2023-04-13 10:11:53 +0300

    fix: change sentry tag on renderer

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -9,7 +9,7 @@ import 'allotment/dist/style.css';
 
 if (window.configurationStore.isErrorReportingEnabled()) {
   Sentry.init({ dsn: 'https://d28c9ac8891348d0926af5d2b8454988@o4504882077302784.ingest.sentry.io/4504882079531008' });
-  Sentry.setTag('boost.type', 'electron.main');
+  Sentry.setTag('boost.type', 'electron.renderer');
 }
 
 const container = document.getElementById('root')!;
