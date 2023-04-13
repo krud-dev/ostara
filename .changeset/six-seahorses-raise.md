aa109dc8053d889b741a9af8dfb6204878da9ebc
avivm <mo.aviv@gmail.com>
2023-04-13 09:30:39 +0100

    feat: add help button (#315)
    * feat: add help button
    
    close #289
    
    * docs(changeset): Added help button to open relevant documentation for each page

---
+++ .changeset/six-seahorses-raise.md
@@ -0,0 +1,5 @@
+---
+'@krud-dev/boost': minor
+---
+
+Added help button to open relevant documentation for each page
--- src/renderer/routes/urls.ts
+++ src/renderer/routes/urls.ts
@@ -1,6 +1,7 @@
 export type UrlInfo = {
   url: string;
   path: string;
+  helpUrl?: string;
 };
 
 function asUrlInfos<T extends { [key: string]: UrlInfo }>(arg: T): T {
@@ -39,6 +40,7 @@ export const urls = asUrlInfos({
   folderApplications: {
     url: '/navigator/folder/:id/applications',
     path: 'applications',
+    helpUrl: 'https://boost.krud.dev/documentation/folders',
   },
   // Application Root
   application: {
@@ -52,14 +54,17 @@ export const urls = asUrlInfos({
   applicationInstances: {
     url: '/navigator/application/:id/instances',
     path: 'instances',
+    helpUrl: 'https://boost.krud.dev/features/application-features/overview/instances',
   },
   applicationLoggers: {
     url: '/navigator/application/:id/loggers',
     path: 'loggers',
+    helpUrl: 'https://boost.krud.dev/features/application-features/manage/loggers',
   },
   applicationCaches: {
     url: '/navigator/application/:id/caches',
     path: 'caches',
+    helpUrl: 'https://boost.krud.dev/features/application-features/manage/caches',
   },
   // Instance Root
   instance: {
@@ -69,46 +74,57 @@ export const urls = asUrlInfos({
   instanceDashboard: {
     url: '/navigator/instance/:id/dashboard',
     path: 'dashboard',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/dashboard',
   },
   instanceMetrics: {
     url: '/navigator/instance/:id/metrics',
     path: 'metrics',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/metrics',
   },
   instanceHttpRequests: {
     url: '/navigator/instance/:id/http-requests',
     path: 'http-requests',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/http-requests',
   },
   instanceQuartz: {
     url: '/navigator/instance/:id/quartz',
     path: 'quartz',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/quartz',
   },
   instanceScheduledTasks: {
     url: '/navigator/instance/:id/scheduled-tasks',
     path: 'scheduled-tasks',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/scheduled-tasks',
   },
   instanceMappings: {
     url: '/navigator/instance/:id/mappings',
     path: 'mappings',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/mappings',
   },
   instanceFlyway: {
     url: '/navigator/instance/:id/flyway',
     path: 'flyway',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/flyway',
   },
   instanceLiquibase: {
     url: '/navigator/instance/:id/liquibase',
     path: 'liquibase',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/liquibase',
   },
   instanceEnvironment: {
     url: '/navigator/instance/:id/environment',
     path: 'environment',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/system-environment',
   },
   instanceSystemEnvironment: {
     url: '/navigator/instance/:id/system-environment',
     path: 'system-environment',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/system-environment',
   },
   instanceBeans: {
     url: '/navigator/instance/:id/beans',
     path: 'beans',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/beans',
   },
   instanceBeansGraph: {
     url: '/navigator/instance/:id/beans-graph',
@@ -117,29 +133,36 @@ export const urls = asUrlInfos({
   instanceProperties: {
     url: '/navigator/instance/:id/properties',
     path: 'properties',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/app-properties',
   },
   instanceSystemProperties: {
     url: '/navigator/instance/:id/system-properties',
     path: 'system-properties',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/system-properties',
   },
   instanceIntegrationGraph: {
     url: '/navigator/instance/:id/integration-graph',
     path: 'integration-graph',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/insights/integration-graph',
   },
   instanceLoggers: {
     url: '/navigator/instance/:id/loggers',
     path: 'loggers',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/manage/loggers',
   },
   instanceCaches: {
     url: '/navigator/instance/:id/caches',
     path: 'caches',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/manage/caches',
   },
   instanceThreadDump: {
     url: '/navigator/instance/:id/thread-dump',
     path: 'thread-dump',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/jvm/thread-profiling',
   },
   instanceHeapDump: {
     url: '/navigator/instance/:id/heap-dump',
     path: 'heap-dump',
+    helpUrl: 'https://boost.krud.dev/features/instance-features/jvm/heap-dump',
   },
 });
