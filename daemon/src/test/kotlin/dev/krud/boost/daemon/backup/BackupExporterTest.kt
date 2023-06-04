21120f7e1b0b5631786f8d3c9f027457f2b9237a
Idan Elhalwani <idan@elhalwani.com>
2023-06-04 13:15:59 +0300

    Exporte/rimporter tests

--- daemon/src/test/kotlin/dev/krud/boost/daemon/backup/BackupExporterTest.kt
+++ daemon/src/test/kotlin/dev/krud/boost/daemon/backup/BackupExporterTest.kt
@@ -3,13 +3,16 @@ package dev.krud.boost.daemon.backup
 import dev.krud.boost.daemon.backup.migration.BackupMigration
 import dev.krud.boost.daemon.configuration.application.entity.Application
 import dev.krud.boost.daemon.configuration.application.stubApplication
+import dev.krud.boost.daemon.configuration.authentication.Authentication
 import dev.krud.boost.daemon.configuration.folder.entity.Folder
 import dev.krud.boost.daemon.configuration.folder.stubFolder
 import dev.krud.boost.daemon.configuration.instance.entity.Instance
 import dev.krud.boost.daemon.configuration.instance.stubInstance
 import dev.krud.boost.daemon.test.TestKrud
-import dev.krud.crudframework.crud.handler.krud.Krud
 import org.junit.jupiter.api.Test
+import strikt.api.expect
+import strikt.assertions.hasSize
+import strikt.assertions.isEqualTo
 import java.util.*
 
 class BackupExporterTest {
@@ -18,9 +21,9 @@ class BackupExporterTest {
             override val toVersion: Int = 1
         }
     )
-    private val folderKrud: Krud<Folder, UUID> = TestKrud(Folder::class.java) { UUID.randomUUID() }
-    private val applicationKrud: Krud<Application, UUID> = TestKrud(Application::class.java) { UUID.randomUUID() }
-    private val instanceKrud: Krud<Instance, UUID> = TestKrud(Instance::class.java) { UUID.randomUUID() }
+    private val folderKrud = TestKrud(Folder::class.java) { UUID.randomUUID() }
+    private val applicationKrud = TestKrud(Application::class.java) { UUID.randomUUID() }
+    private val instanceKrud = TestKrud(Instance::class.java) { UUID.randomUUID() }
     private val backupExporter = BackupExporter(
         backupMigrations,
         folderKrud,
@@ -30,20 +33,35 @@ class BackupExporterTest {
 
     @Test
     fun `exportCurrentConfigurations happy flow`() {
-        val rootFolder1 = folderKrud.create(stubFolder(alias = "rootFolder1")).apply {
+        folderKrud.create(stubFolder(alias = "rootFolder1")).apply {
             folderKrud.create(stubFolder(alias = "rootFolder1_1", parentFolderId = this.id))
-            folderKrud.create(stubFolder(alias = "rootFolder1_2", parentFolderId = this.id))
-            applicationKrud.create(stubApplication(alias = "rootFolder1Application1", parentFolderId = this.id)).apply {
-                instanceKrud.create(stubInstance(alias = "rootFolder1ApplicationInstance1", parentApplicationId = this.id))
-                instanceKrud.create(stubInstance(alias = "rootFolder1ApplicationInstance2", parentApplicationId = this.id))
+            applicationKrud.create(stubApplication(alias = "rootFolder1_1Application1", parentFolderId = this.id, authentication = Authentication.Basic("test", "test"))).apply {
+                instanceKrud.create(stubInstance(alias = "rootFolder1_1Application1Instance1", parentApplicationId = this.id))
             }
         }
-        val rootApplication1 = applicationKrud.create(stubApplication(alias = "rootApplication1")).apply {
-            instanceKrud.create(stubInstance(alias = "rootApplicationInstance1", parentApplicationId = this.id))
-            instanceKrud.create(stubInstance(alias = "rootApplicationInstance2", parentApplicationId = this.id))
-        }
         val dto = backupExporter.exportAll()
-
-        // todo: assertions
+        expect {
+            that(dto.version).isEqualTo(1)
+            that(dto.tree).hasSize(1)
+            that(dto.tree[0].type).isEqualTo("folder")
+            val rootFolder = dto.tree[0] as BackupDTO.TreeElement.Folder
+            that(rootFolder.model.alias).isEqualTo("rootFolder1")
+            that(rootFolder.children).hasSize(2)
+            that(rootFolder.children[0].type).isEqualTo("folder")
+            that(rootFolder.children[1].type).isEqualTo("application")
+            val application = rootFolder.children[1] as BackupDTO.TreeElement.Application
+            that(application.model.alias).isEqualTo("rootFolder1_1Application1")
+            that(application.model.authenticationProperties).isEqualTo(
+                mapOf(
+                    "type" to "basic",
+                    "username" to "test",
+                    "password" to "test"
+                )
+            )
+            that(application.children).hasSize(1)
+            that(application.children[0].type).isEqualTo("instance")
+            val instance = application.children[0] as BackupDTO.TreeElement.Application.Instance
+            that(instance.model.alias).isEqualTo("rootFolder1_1Application1Instance1")
+        }
     }
 }
