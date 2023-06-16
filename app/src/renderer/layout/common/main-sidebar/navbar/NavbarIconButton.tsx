8c0146dff9b925924a97ca062f01c64a2fcd81ce
avivm <mo.aviv@gmail.com>
2023-06-16 15:46:50 +0300

    fix: settings navbar button behavior

--- app/src/renderer/layout/common/main-sidebar/navbar/NavbarIconButton.tsx
+++ app/src/renderer/layout/common/main-sidebar/navbar/NavbarIconButton.tsx
@@ -1,22 +1,23 @@
 import { Box, IconButton, Tooltip } from '@mui/material';
-import { IconViewer, MUIconType } from '../../../../components/common/IconViewer';
+import { IconViewer, MUIconType } from 'renderer/components/common/IconViewer';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 type NavbarIconButtonProps = {
   titleId: string;
   icon: MUIconType;
+  active?: boolean;
   onClick?: () => void;
 };
 
 export const NAVBAR_TOOLTIP_DELAY = 1000;
 
-export default function NavbarIconButton({ titleId, icon, onClick }: NavbarIconButtonProps) {
+export default function NavbarIconButton({ titleId, icon, active, onClick }: NavbarIconButtonProps) {
   return (
     <Box sx={{ '-webkit-app-region': 'no-drag' }}>
       <Tooltip title={<FormattedMessage id={titleId} />} enterDelay={NAVBAR_TOOLTIP_DELAY}>
-        <IconButton size={'small'} onClick={onClick} sx={{ color: 'text.primary' }}>
-          <IconViewer icon={icon} fontSize={'medium'} sx={{ color: 'text.primary' }} />
+        <IconButton size={'small'} onClick={onClick} sx={{ color: active ? 'primary.main' : 'text.primary' }}>
+          <IconViewer icon={icon} fontSize={'medium'} />
         </IconButton>
       </Tooltip>
     </Box>
--- app/src/renderer/layout/common/main-sidebar/navbar/SettingsMenu.tsx
+++ app/src/renderer/layout/common/main-sidebar/navbar/SettingsMenu.tsx
@@ -1,16 +1,41 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSubscribeToEvent } from 'renderer/apis/requests/subscriptions/subscribeToEvent';
 import { IpcRendererEvent } from 'electron';
 import NavbarIconButton from './NavbarIconButton';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { urls } from 'renderer/routes/urls';
+import { getUrlInfo } from 'renderer/utils/urlUtils';
 
 export default function SettingsMenu() {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  const openSettingsHandler = useCallback((): void => {
-    navigate(urls.settings.url);
+  const [settingsPagesCounter, setSettingsPagesCounter] = useState<number>(0);
+
+  const isSettingsPathname = useCallback((pathnameToCheck: string): boolean => {
+    return pathnameToCheck.startsWith(urls.settings.url);
   }, []);
+
+  useEffect(() => {
+    if (isSettingsPathname(pathname)) {
+      const urlInfo = getUrlInfo(pathname);
+      if (!urlInfo?.redirect) {
+        setSettingsPagesCounter((prev) => prev + 1);
+      }
+    } else {
+      setSettingsPagesCounter(0);
+    }
+  }, [pathname]);
+
+  const active = useMemo<boolean>(() => isSettingsPathname(pathname), [pathname]);
+
+  const toggleSettingsHandler = useCallback((): void => {
+    if (isSettingsPathname(pathname)) {
+      navigate(-settingsPagesCounter);
+    } else {
+      navigate(urls.settings.url);
+    }
+  }, [pathname, settingsPagesCounter]);
 
   const subscribeToTriggerEventsState = useSubscribeToEvent();
 
@@ -20,7 +45,7 @@ export default function SettingsMenu() {
       unsubscribe = await subscribeToTriggerEventsState.mutateAsync({
         event: 'trigger:openSettings',
         listener: (event: IpcRendererEvent) => {
-          openSettingsHandler();
+          toggleSettingsHandler();
         },
       });
     })();
@@ -29,5 +54,7 @@ export default function SettingsMenu() {
     };
   }, []);
 
-  return <NavbarIconButton titleId={'settings'} icon={'SettingsOutlined'} onClick={openSettingsHandler} />;
+  return (
+    <NavbarIconButton titleId={'settings'} icon={'SettingsOutlined'} active={active} onClick={toggleSettingsHandler} />
+  );
 }
