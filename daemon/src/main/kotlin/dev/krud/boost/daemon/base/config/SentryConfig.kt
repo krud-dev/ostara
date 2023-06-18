97310ec885f06475d38c3135e06de12cfa430e40
Idan Elhalwani <idan@elhalwani.com>
2023-06-18 09:05:24 +0300

    chore: add version to sentry on daemon

--- daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/SentryConfig.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/SentryConfig.kt
@@ -2,14 +2,15 @@ package dev.krud.boost.daemon.base.config
 
 import io.sentry.Sentry
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SentryConfig {
-
     @Bean
-    fun applySentryTags() = CommandLineRunner {
+    fun applySentryTags(buildProperties: BuildProperties) = CommandLineRunner {
         Sentry.setTag("service.type", "daemon")
+        Sentry.setTag("daemon.version", buildProperties.version)
     }
 }
