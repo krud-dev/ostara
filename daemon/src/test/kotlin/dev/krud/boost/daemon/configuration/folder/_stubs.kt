df793a48acdc13e5f6df526d8afaa978da54432a
Idan Elhalwani <idan@elhalwani.com>
2023-06-21 10:54:03 +0300

    fix: add missing sort when exporting folders

--- daemon/src/test/kotlin/dev/krud/boost/daemon/configuration/folder/_stubs.kt
+++ daemon/src/test/kotlin/dev/krud/boost/daemon/configuration/folder/_stubs.kt
@@ -4,8 +4,8 @@ import dev.krud.boost.daemon.configuration.authentication.Authentication
 import dev.krud.boost.daemon.configuration.folder.entity.Folder
 import java.util.*
 
-fun stubFolder(id: UUID = UUID.randomUUID(), alias: String = "stubFolder", authentication: Authentication = Authentication.Inherit.DEFAULT, parentFolderId: UUID? = null): Folder {
-    return Folder(alias, authentication = authentication, parentFolderId = parentFolderId).apply {
+fun stubFolder(id: UUID = UUID.randomUUID(), alias: String = "stubFolder", authentication: Authentication = Authentication.Inherit.DEFAULT, parentFolderId: UUID? = null, sort: Double = 1.0): Folder {
+    return Folder(alias, authentication = authentication, parentFolderId = parentFolderId, sort = sort).apply {
         this.id = id
     }
 }
