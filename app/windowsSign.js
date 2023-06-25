6fff389a722a41fc82c448c34796b654421b394d
Idan Elhalwani <idan@elhalwani.com>
2023-06-25 09:59:32 +0300

    Change to spawn

--- app/windowsSign.js
+++ app/windowsSign.js
@@ -1,20 +1,23 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 exports.default = async function (configuration) {
     const { path } = configuration;
     const cmd = `signtool sign /n "Open Source Developer, Shani Holdengreber" /t http://time.certum.pl /fd sha256 /v ${path}`;
     console.log(`--- signing cmd: ${cmd}`);
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`--- signing error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            console.log(`--- signing stderr: ${stderr}`);
-            return;
-        }
-
-        console.log(`--- signing stdout: ${stdout}`);
+    const execution = spawn(cmd, { shell: true });
+    ls.stdout.on("data", data => {
+        console.log(`stdout: ${data}`);
+    });
+    
+    ls.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+    
+    ls.on('error', (error) => {
+        console.log(`error: ${error.message}`);
+    });
+    
+    ls.on("close", code => {
+        console.log(`child process exited with code ${code}`);
     });
 }
