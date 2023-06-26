55a399d0e53bdcee4d60692e4d4f9877915cff7e
Idan Elhalwani <idan@elhalwani.com>
2023-06-26 11:03:15 +0300

    Fix windows code signing (#605)

---
+++ app/windowsSign.js
@@ -0,0 +1,48 @@
+const { spawn } = require("child_process");
+
+exports.default = async function (configuration) {
+    if (process.platform !== "win32") {
+        console.log(`Skipping windows signing for ${configuration.path}`);
+        return;
+    }
+    console.log("Called with " + configuration.path)
+    const { path } = configuration;
+    const cmd = `signtool sign /n "Open Source Developer, Shani Holdengreber" /t http://time.certum.pl /fd sha256 /v "${path}"`;
+    console.log(`--- signing cmd: ${cmd}`);
+    try {
+        await spawnTool(cmd);
+    } catch (e) {
+        if (e.message.includes("The file is being used by another process") || e.message.includes("The specified timestamp server either could not be reached")) {
+            console.log("Retrying in 5 seconds");
+            await new Promise((resolve, reject) => {
+                setTimeout(async () => {
+                    await spawnTool(cmd);
+                    resolve();
+                }, 5000);
+            });
+        }
+    }
+}
+
+function spawnTool(command) {
+    return new Promise((resolve, reject) => {
+        const execution = spawn(command, { shell: true });
+        execution.stdout.on("data", data => {
+            console.log(`stdout: ${data}`);
+        });
+
+        execution.stderr.on("data", data => {
+            console.log(`stderr: ${data}`);
+        });
+
+        execution.on('error', (error) => {
+            console.log(`error: ${error.message}`);
+            reject(error);
+        });
+
+        execution.on("close", code => {
+            console.log(`child process exited with code ${code}`);
+            resolve();
+        });
+    });
+}
