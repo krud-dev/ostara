c4c1da63dad612ad9f58a01bb2f10b4fdb0b2c90
Idan Elhalwani <idan@elhalwani.com>
2023-06-27 11:56:54 +0300

    Stub

--- daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/cache/CacheProperties.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/cache/CacheProperties.kt
@@ -1,5 +1,6 @@
 package dev.krud.boost.daemon.base.config.cache
 
+import dev.krud.boost.daemon.base.annotations.GenerateTypescript
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
@@ -10,6 +11,7 @@ class CacheProperties {
     var caches: List<CacheDTO> = listOf()
 
     companion object {
+        @GenerateTypescript
         data class CacheDTO(
             val name: String,
             val expireAfterWrite: Duration?,
