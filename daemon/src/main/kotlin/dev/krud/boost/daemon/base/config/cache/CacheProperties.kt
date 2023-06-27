df9f75f10121dc134c009474c51e79efd33bc31c
Idan Elhalwani <idan@elhalwani.com>
2023-06-27 13:23:21 +0300

    Remove disparity

--- daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/cache/CacheProperties.kt
+++ daemon/src/main/kotlin/dev/krud/boost/daemon/base/config/cache/CacheProperties.kt
@@ -1,6 +1,5 @@
 package dev.krud.boost.daemon.base.config.cache
 
-import dev.krud.boost.daemon.base.annotations.GenerateTypescript
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
@@ -11,7 +10,6 @@ class CacheProperties {
     var caches: List<CacheDTO> = listOf()
 
     companion object {
-        @GenerateTypescript
         data class CacheDTO(
             val name: String,
             val expireAfterWrite: Duration?,
