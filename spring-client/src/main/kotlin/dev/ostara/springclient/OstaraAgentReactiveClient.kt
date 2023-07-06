270b5217ba9cb7ca86e82d75de84f2761d111777
Idan Elhalwani <idan@elhalwani.com>
2023-07-06 14:12:14 +0300

    fix(client): fix agent urls (#674)

--- spring-client/src/main/kotlin/dev/ostara/springclient/OstaraAgentReactiveClient.kt
+++ spring-client/src/main/kotlin/dev/ostara/springclient/OstaraAgentReactiveClient.kt
@@ -1,14 +1,20 @@
 package dev.ostara.springclient
 
+import dev.ostara.springclient.util.DEREGISTRATION_ENDPOINT
+import dev.ostara.springclient.util.REGISTRATION_ENDPOINT
 import org.springframework.web.reactive.function.client.WebClient
 
 class OstaraAgentReactiveClient(
-  private val url: String,
+  private val baseUrl: String,
   private val webClient: WebClient
 ) : OstaraAgentClient {
+  private val registrationUrl = "${baseUrl.removeSuffix("/")}${REGISTRATION_ENDPOINT}"
+  private val deregistrationUrl = "${baseUrl.removeSuffix("/")}${DEREGISTRATION_ENDPOINT}}"
+
   override fun register(request: RegistrationRequest) {
+
     webClient.post()
-      .uri("$url/api/v1/service-discovery/internal/register")
+      .uri(registrationUrl)
       .bodyValue(request)
       .retrieve()
       .bodyToMono(String::class.java)
@@ -17,7 +23,7 @@ class OstaraAgentReactiveClient(
 
   override fun deregister(request: RegistrationRequest) {
     webClient.post()
-      .uri("$url/api/v1/service-discovery/internal/deregister")
+      .uri(deregistrationUrl)
       .bodyValue(request)
       .retrieve()
       .bodyToMono(String::class.java)
