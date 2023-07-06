f1b1a84dc773ce63ff4341ed00e73de9e5482844
Idan Elhalwani <idan@elhalwani.com>
2023-07-06 14:11:13 +0300

    fix(client): fix agent urls

--- spring-client/src/main/kotlin/dev/ostara/springclient/OstaraAgentWebClient.kt
+++ spring-client/src/main/kotlin/dev/ostara/springclient/OstaraAgentWebClient.kt
@@ -1,27 +1,32 @@
 package dev.ostara.springclient
 
+import dev.ostara.springclient.util.DEREGISTRATION_ENDPOINT
+import dev.ostara.springclient.util.REGISTRATION_ENDPOINT
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
 
 class OstaraAgentWebClient(
-  private val url: String,
+  private val baseUrl: String,
   private val restTemplate: RestTemplate
 ) : OstaraAgentClient {
+  private val registrationUrl = "${baseUrl.removeSuffix("/")}$REGISTRATION_ENDPOINT"
+  private val deregistrationUrl = "${baseUrl.removeSuffix("/")}$DEREGISTRATION_ENDPOINT}"
+
   override fun register(request: RegistrationRequest) {
     val response = restTemplate.exchange(
-      "$url/api/v1/service-discovery/internal/register",
-        HttpMethod.POST,
-        HttpEntity(request),
+      registrationUrl,
+      HttpMethod.POST,
+      HttpEntity(request),
       String::class.java
     )
   }
 
   override fun deregister(request: RegistrationRequest) {
     val response = restTemplate.exchange(
-      "$url/api/v1/service-discovery/internal/deregister",
-        HttpMethod.POST,
-        HttpEntity(request),
+      deregistrationUrl,
+      HttpMethod.POST,
+      HttpEntity(request),
       String::class.java
     )
   }
--- spring-client/src/main/kotlin/dev/ostara/springclient/util/_constants.kt
+++ spring-client/src/main/kotlin/dev/ostara/springclient/util/_constants.kt
@@ -1,3 +1,7 @@
 package dev.ostara.springclient.util
 
 const val CONFIGURATION_PREFIX = "ostara.client"
+
+const val API_PREFIX = "/api/v1"
+const val REGISTRATION_ENDPOINT = "$API_PREFIX/internal/service-discovery/register"
+const val DEREGISTRATION_ENDPOINT = "$API_PREFIX/internal/service-discovery/deregister"
