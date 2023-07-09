2afecb6d362c7daa8bf628542be6f6ceeb71b528
Idan Elhalwani <idan@elhalwani.com>
2023-07-09 13:20:31 +0300

    Fix broken test

--- agent/src/main/kotlin/dev/ostara/agent/controller/MainController.kt
+++ agent/src/main/kotlin/dev/ostara/agent/controller/MainController.kt
@@ -1,7 +1,7 @@
 package dev.ostara.agent.controller
 
 import dev.ostara.agent.config.ServiceDiscoveryProperties
-import dev.ostara.agent.config.ServiceDiscoveryProperties.Companion.serviceDiscoveriesWithoutInternal
+import dev.ostara.agent.config.ServiceDiscoveryProperties.Companion.serviceDiscoveries
 import dev.ostara.agent.model.AgentInfoDTO
 import dev.ostara.agent.util.API_PREFIX
 import org.springframework.boot.info.BuildProperties
@@ -19,7 +19,7 @@ class MainController(
   fun getInfo(): AgentInfoDTO {
     return AgentInfoDTO(
       version = buildProperties.version,
-      sources = serviceDiscoveryProperties.serviceDiscoveriesWithoutInternal.map { it::class.simpleName!! }.toSet()
+      sources = serviceDiscoveryProperties.serviceDiscoveries.map { it::class.simpleName!! }.toSet()
     )
   }
 }
