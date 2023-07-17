d9adaebb5818b39dc158ffef6193abc9db916ea5
Idan Elhalwani <idan@elhalwani.com>
2023-07-17 08:59:32 +0300

    chore: add project name to spring client pom

--- spring-client/build.gradle.kts
+++ spring-client/build.gradle.kts
@@ -88,6 +88,7 @@ if (hasProperty("release")) {
       from(components["java"])
       version = effectiveVersion
       pom {
+        name = project.name
         version = effectiveVersion
         description.set("The Spring Client for Ostara, a cross-platform desktop app for managing and monitoring Spring Boot applications using the Actuator API, providing comprehensive insights and effortless control.")
         url.set("https://github.com/krud-dev/ostara")
